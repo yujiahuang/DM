bfe832b829f4333df7414acd41658f02716657bb
Kevin Huang <khdreamer@ubuntu.(none)>
2015-01-23 02:13:05 +0800

    one-class test

--- src/exec_svm.sh
+++ src/exec_svm.sh
@@ -1,3 +1,3 @@
 lib/libsvm/tools/subset.py -s 0 data/feature_data 1600 data/train_data data/test_data
-lib/libsvm/svm-train -s 2 -g 0.4 -n 0.1 data/train_data
-lib/libsvm/svm-predict data/test_data train_data.model predict_output
+lib/libsvm/svm-train -s 2 -g 0.8 -n 0.068 data/train_data
+lib/libsvm/svm-predict data/test_data_all_p train_data.model predict_output
