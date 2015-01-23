677b2d5935348ddacba528f803b96f1bb8ab3510
Kevin Huang <khdreamer@ubuntu.(none)>
2015-01-24 01:31:40 +0800

    exec svm

--- src/exec_svm.sh
+++ src/exec_svm.sh
@@ -1,5 +1,13 @@
-lib/libsvm/tools/subset.py -s 0 data/feature_data 1600 data/train_data data/test_data
-lib/libsvm/svm-train -s 2 -g 0.8 -n 0.066 data/train_data
+lib/libsvm/tools/subset.py -s 0 data/feature_data $1 data/train_data data/test_data
+
+python gen_half_data.py data/train_data data/train_data_half
+python gen_half_data.py data/test_data data/test_data_half
+
+lib/libsvm/svm-scale -u 10 -l -10 data/train_data_half > data/train_data_half_scale
+lib/libsvm/svm-scale -u 10 -l -10 data/test_data_half > data/test_data_half_scale
+
+# lib/libsvm/svm-train -s 2 -g 0.8 -n 0.066 data/train_data
 # lib/libsvm/svm-train -s 2 -g 0.0001 -n 0.5 data/train_data
 # lib/libsvm/svm-train -s 0 -c 100000 data/train_data
-lib/libsvm/svm-predict data/test_data train_data.model predict_output
+lib/libsvm/svm-train -g 3.0517578125e-05 -c 32768 data/train_data_half_scale
+lib/libsvm/svm-predict data/test_data_half_scale train_data_half_scale.model predict_output
