522460b6ab939ed74a8be3f6cf077a2158284622
Kevin Huang <khdreamer@ubuntu.(none)>
2015-01-24 01:33:23 +0800

    gen half data

---
+++ src/gen_half_data.py
@@ -0,0 +1,15 @@
+import sys
+
+test_data = open(sys.argv[1], 'r')
+output = open(sys.argv[2], 'w+')
+
+c = 0
+for line in test_data:
+  tokens = line.split(' ')
+  if tokens[0] == '+1':
+  	output.write(line)
+  else:
+  	if c%8 == 0:
+  	  output.write(line)
+  	  # print c
+  	c+=1
