dd4f815194999b0b3c810027dd84fcc6ce7a8dfe
Kevin Huang <kevinhuangdreamer@gmail.com>
2015-01-24 10:31:22 +0800

    medium scale test

--- src/exec_svm.sh
+++ src/exec_svm.sh
@@ -9,5 +9,5 @@ lib/libsvm/svm-scale -u 10 -l -10 data/test_data_half > data/test_data_half_scal
 # lib/libsvm/svm-train -s 2 -g 0.8 -n 0.066 data/train_data
 # lib/libsvm/svm-train -s 2 -g 0.0001 -n 0.5 data/train_data
 # lib/libsvm/svm-train -s 0 -c 100000 data/train_data
-lib/libsvm/svm-train -g 3.0517578125e-05 -c 32768 data/train_data_half_scale
+lib/libsvm/svm-train -g 3.0517578125e-05 -c 128 data/train_data_half_scale
 lib/libsvm/svm-predict data/test_data_half_scale train_data_half_scale.model predict_output
